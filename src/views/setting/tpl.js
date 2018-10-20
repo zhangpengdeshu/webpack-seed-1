b4910eb3f3dbdf4e711a668fa3cda39e7faffa0f
loadingmore <biyuqiwan@163.com>
2018-10-20 18:08:20 +0800

    new branch for web-pc site demo

--- src/views/setting/tpl.js
+++
@@ -1,7 +0,0 @@
-const content = require('./setting.ejs')
-const layout = require('layout/layout/layout.js')
-const pageTitle = '设置'
-
-const temp = layout.init({pageTitle}).run(content({pageTitle}))
-
-module.exports = temp
