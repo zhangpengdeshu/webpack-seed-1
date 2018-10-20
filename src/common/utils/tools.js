1b1c5223fa9c60c9c0523532ae85193768d35280
biyuqi <biyuqiwan@163.com>
2018-10-20 22:24:30 +0800

    :memo: 编写首页

--- src/common/utils/tools.js
+++ src/common/utils/tools.js
@@ -1,9 +1,1 @@
-export const Setting = () => {
-  return '设置页面'
-}
-export const About = () => {
-  return '关于页面'
-}
-export const indexPage = () => {
-  return '首页'
-}
+// 工具函数可在此处填写
--- src/views/index/index.js
+++ src/views/index/index.js
@@ -2,7 +2,4 @@
  * 默认样式+默认逻辑
  */
 import '@/common/js/base'
-// 测试用的工具函数
-import { indexPage } from 'utils/tools'
-
-document.querySelector('.index-title').innerHTML = indexPage()
+import './index.scss'
